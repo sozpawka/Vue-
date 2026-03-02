40cd107c4e302661d7888319f0fbc15236b00add
Sozpawka <sozpawkawar@gmail.com>
2026-03-02 10:55:07 +0700

    Практическое задание 1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let product = "Socks";
+let description = "A pair of warm, fuzzy socks";//1 практическая
+
+let app = new Vue({
+   el: '#app',
+   data: {
+       product: "Socks",
+       description: "A pair of warm, fuzzy socks" 
+   }
+})
