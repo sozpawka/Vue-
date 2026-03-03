24605641ccf3d45288a48f07065437692d10f53f
Sozpawka <sozpawkawar@gmail.com>
2026-03-03 13:40:52 +0700

    10 практическая

--- js/main.js
+++ js/main.js
@@ -1,6 +1,87 @@
 
+Vue.component('product-review', {
+   template: `
+   <form class="review-form" @submit.prevent="onSubmit">
+    <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+            <li v-for="error in errors">{{ error }}</li>
+        </ul>
+    </p>
+    <p>Would you recommend this product?</p> <!-- 10 практическая -->
 
-Vue.component('product-details', { // 8 практическая
+        <label>
+        <input type="radio" value="Yes" v-model="recommend">
+            Yes
+        </label>
+
+        <label>
+        <input type="radio" value="No" v-model="recommend">
+            No
+        </label>
+     <p>
+       <label for="name">Name:</label>
+       <input id="name" v-model="name" placeholder="name" required>
+     </p>
+
+     <p>
+       <label for="review">Review:</label>
+       <textarea id="review" v-model="review"></textarea>
+     </p>
+
+     <p>
+       <label for="rating">Rating:</label>
+       <select id="rating" v-model.number="rating">
+         <option>5</option>
+         <option>4</option>
+         <option>3</option>
+         <option>2</option>
+         <option>1</option>
+       </select>
+     </p>
+
+     <p>
+       <input type="submit" value="Submit"> 
+     </p>
+   </form>
+ `,
+   data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            errors: [],
+            recommend: null,
+        }
+    },
+    methods:{
+        onSubmit() {
+            if(this.name && this.review && this.rating && this.recommend) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend,
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+                this.recommend = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+                if(!this.recommend) this.errors.push("Recommendation required.")
+            }
+        }
+
+    }
+});
+
+
+// 8 практическая
+Vue.component('product-details', {
     props: ['details'],
     template: `
       <ul>
@@ -15,22 +96,20 @@ Vue.component('product', {
            type: Boolean,
            required: true
        },
-
    },
     template: `
         <div class="product">
             <div class="product-image">
-                <img v-bind:src="image" v-bind:alt="altText" />
+                <img :src="image" :alt="altText" />
             </div>
-
             <div class="product-info">
                 <h1>{{ title }}</h1>
-                <p> {{description}}</p>
+                <p>{{ description }}</p>
                 <p v-if="inventory > 10">In stock</p>
                 <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
                 <p v-else>Out of stock</p>
-                <a v-bind:href="link">More products like this</a>
-                <p> {{ sale }}</p>
+                <a :href="link">More products like this</a>
+                <p>{{ sale }}</p>
                 <span v-show="onSale">On Sale</span>
                 <span v-show="!onSale">Not Sale</span>
                 <product-details :details="details"></product-details>
@@ -42,7 +121,7 @@ Vue.component('product', {
                     :style="{ backgroundColor:variant.variantColor }"
                     @mouseover="updateProduct(index)"
                 ></div>
-                <p> Sizes: </p>
+                <p>Sizes:</p>
                 <ul>
                     <li v-for="size in sizes">{{ size }}</li> <!-- 4 практическая -->
                 </ul>
@@ -53,13 +132,32 @@ Vue.component('product', {
                     Out of Stock 
                 </p><!-- 6 практическая -->
                 <button
-                    v-on:click="addToCart"
+                    @click="addToCart"
                     :disabled="!inStock"
                     :class="{ disabledButton: !inStock }"
                 >
                     Add to cart
                 </button>
-                <button v-on:click="DeleteToCart">Delete to cart</button>
+
+                <button @click="deleteFromCart">
+                    Delete to cart
+                </button>
+                <div>
+                    <h2>Reviews</h2>
+                    <p v-if="!reviews.length">There are no reviews yet.</p>
+                    <ul>
+                        <li v-for="review in reviews">
+                            <p>{{ review.name }}</p>
+                            <p>Rating: {{ review.rating }}</p>
+                            <p>{{ review.review }}</p>
+                            <p>Recommend: {{ review.recommend }}</p> 
+                        </li>
+                    </ul>
+                </div>
+                <product-review 
+                    @review-submitted="addReview">
+                </product-review>
+
             </div>
         </div>
     `,
@@ -67,7 +165,6 @@ Vue.component('product', {
         return {
             brand: 'Vue Mastery',
             product: "Socks",
-            image: "./assets/vmSocks-blue-onWhite.jpg",
             description: "A pair of warm, fuzzy socks", // 1 практическая
             altText: "A pair of socks",
             link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks", // 2 практическая
@@ -76,6 +173,7 @@ Vue.component('product', {
             onSale: false, // 3 практическая 
             details: ['80% cotton', '20% polyester', 'Gender-neutral'],
             sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            reviews: [],
 
             variants: [
                 {
@@ -99,13 +197,15 @@ Vue.component('product', {
             this.variants[this.selectedVariant].variantId);
         },
 
-        DeleteToCart() {
+        deleteFromCart() {
             this.$emit('delete-to-cart',
             this.variants[this.selectedVariant].variantId);
         },
         updateProduct(index) {
             this.selectedVariant = index;
-            console.log(index);
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
         }
     },
     computed: {
@@ -120,9 +220,9 @@ Vue.component('product', {
         },
         sale() { // 7 практическая
             if (this.onSale) {
-                return this.brand + ' ' + this.product + 'on sale'
+                return this.brand + ' ' + this.product + ' on sale'
             } else {
-                return this.brand + ' ' + this.product + 'not sale'
+                return this.brand + ' ' + this.product + ' not sale'
             }
         },
         shipping() {
@@ -132,7 +232,6 @@ Vue.component('product', {
                 return 2.99
             }
         }
-
     },
 });
 
@@ -153,5 +252,4 @@ new Vue({
             }
         }
     }
-
 });
